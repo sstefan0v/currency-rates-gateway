bf9690aaa963f421e1f937d23b94c5661df3fa1f
Stefan Stefanov <elken@abv.bg>
2025-03-13 17:26:47 +0200

    add gitignore, properties, docker

---
+++ src/main/java/com/sstefanov/currencies/gateway/Application.java
@@ -0,0 +1,14 @@
+package com.sstefanov.currencies.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+//@EnableConfigurationProperties({BnbUrlProps.class, WebSocketProps.class})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
