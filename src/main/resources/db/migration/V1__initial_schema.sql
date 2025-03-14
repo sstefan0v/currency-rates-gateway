e83c37d7eeabe42c1037d124cc7a112d0a965d1d
Stefan Stefanov <elken@abv.bg>
2025-03-14 16:38:07 +0200

    clean up

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,19 +1,20 @@
 CREATE TABLE request_statistics
 (
     id           SERIAL PRIMARY KEY,
-    service_name VARCHAR(20) NOT NULL,
+    service_name VARCHAR(20)        NOT NULL,
     request_id   VARCHAR(40) UNIQUE NOT NULL,
-    timestamp    BIGINT NOT NULL ,
-    client_id    VARCHAR(25) NOT NULL
+    timestamp    BIGINT             NOT NULL,
+    client_id    VARCHAR(25)        NOT NULL
 );
 
-CREATE TABLE currency_rates (
-                                timestamp BIGINT NOT NULL,
-                                base_currency CHAR(3) NOT NULL,
-                                currency CHAR(3) NOT NULL,
-                                rate NUMERIC(20,10) NOT NULL,
-                                PRIMARY KEY (timestamp, base_currency, currency)
+CREATE TABLE currency_rates
+(
+    timestamp     BIGINT          NOT NULL,
+    base_currency CHAR(3)         NOT NULL,
+    currency      CHAR(3)         NOT NULL,
+    rate          NUMERIC(20, 10) NOT NULL,
+    PRIMARY KEY (timestamp, base_currency, currency)
 ) PARTITION BY RANGE (timestamp);
 
 CREATE TABLE currency_rates_2025 PARTITION OF currency_rates
-FOR VALUES FROM (1735682400000) TO (1767218399000);
+    FOR VALUES FROM 1735682400000 TO 1767218399000;
