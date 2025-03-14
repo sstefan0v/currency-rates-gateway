d62031c1fa88f747f29921f3a6c1edcca11a7605
Stefan Stefanov <elken@abv.bg>
2025-03-14 15:12:43 +0200

    add flyway config and pom

--- src/main/java/com/sstefanov/currencies/gateway/Application.java
+++ src/main/java/com/sstefanov/currencies/gateway/Application.java
@@ -3,10 +3,12 @@ package com.sstefanov.currencies.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableCaching
-//@EnableConfigurationProperties({BnbUrlProps.class, WebSocketProps.class})
+@EnableWebFlux
+//@EnableConfigurationProperties({.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
