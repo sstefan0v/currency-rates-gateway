ae70936d9d4ac844c069c94921fcaa98b866d95a
Stefan Stefanov <elken@abv.bg>
2025-03-16 13:09:53 +0200

    clean up

--- src/main/java/com/sstefanov/currencies/gateway/service/GatewayService.java
+++ src/main/java/com/sstefanov/currencies/gateway/service/GatewayService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-
 public class GatewayService {
 
     private final RequestStatisticsSaver requestStatisticsSaver;
--- src/main/java/com/sstefanov/currencies/gateway/service/StatisticsRabbitMqSender.java
+++ src/main/java/com/sstefanov/currencies/gateway/service/StatisticsRabbitMqSender.java
@@ -3,10 +3,8 @@ package com.sstefanov.currencies.gateway.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
@@ -39,15 +37,5 @@ public class StatisticsRabbitMqSender {
 
         log.info("Sending message to RabbitMQ, Stats: {}", message);
         rabbitTemplate.send(new Message(message.getBytes(StandardCharsets.UTF_8)));
-    }
-
-    @Scheduled(initialDelay = 1000, fixedRate = 1000)
-    private void sendingMessages() {
-        send("requestId", "clientId", "serviceName", Long.toString(2));
-    }
-
-    @RabbitListener(queues = "rates")
-    private void receiveMessage(String message) {
-        System.out.println("Received: " + message);
     }
 }
