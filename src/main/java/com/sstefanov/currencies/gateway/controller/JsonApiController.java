bbef3f99ed25d5784219ec8ec681e1b08b12582e
Stefan Stefanov <elken@abv.bg>
2025-03-16 10:59:11 +0200

    add gatewayService adn jsoncontroller

---
+++ src/main/java/com/sstefanov/currencies/gateway/controller/JsonApiController.java
@@ -0,0 +1,52 @@
+package com.sstefanov.currencies.gateway.controller;
+
+import com.sstefanov.currencies.gateway.controller.model.json.request.CurrentRequest;
+import com.sstefanov.currencies.gateway.controller.model.json.request.HistoryRequest;
+import com.sstefanov.currencies.gateway.service.GatewayService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+
+//@RequestMapping(value = "/json_api" ,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/json_api" , produces = MediaType.APPLICATION_JSON_VALUE)
+public class JsonApiController {
+
+    private final GatewayService gatewayService;
+
+    @PostMapping("/current")
+    public Object getCurrent(@RequestBody CurrentRequest request) {
+        return gatewayService.process(request);
+    }
+
+    @PostMapping("/history")
+    public Object getHistory(@RequestBody HistoryRequest request) {
+        return gatewayService.process(request);
+    }
+
+    @GetMapping("/test/{requestId}/{period}")
+    public Object test(@PathVariable String requestId, @PathVariable Integer period) {
+        CurrentRequest newRequest = new CurrentRequest();
+        newRequest.setRequestId(requestId);
+        newRequest.setTimestamp(System.currentTimeMillis());
+        newRequest.setClient("stefan-client");
+        newRequest.setCurrency("EUR");
+
+        if (period == 69) {
+            return getCurrent(newRequest);
+        }
+        HistoryRequest historyRequest = new HistoryRequest();
+        historyRequest.setRequestId(requestId);
+        historyRequest.setTimestamp(System.currentTimeMillis());
+        historyRequest.setClient("stefan-client");
+        historyRequest.setPeriod(period);
+        historyRequest.setCurrency("EUR");
+        return getHistory(historyRequest);
+
+    }
+}
